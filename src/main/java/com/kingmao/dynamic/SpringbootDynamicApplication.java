d50a19fbc6b388e54ed0488b23d5fa78c2116ecc
qinxuan <yiqinxuan@chainup.com>
2020-04-27 18:02:57 +0800

    init

---
+++ src/main/java/com/kingmao/dynamic/SpringbootDynamicApplication.java
@@ -0,0 +1,13 @@
+package com.kingmao.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDynamicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDynamicApplication.class, args);
+    }
+
+}
